27ed230763488b50297076ea8d12479f510e7b47
Sue <susanhartesquire@yahoo.com>
2019-07-01 18:13:10 -0400

    Commit deletion of extra code and added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-let employees = [];
+let employees = []; //Global variable
 
 // //Gallery Mark-Up: Creating the twelve individual employee boxes/business cards for Getting and displaying 12 Random Users
-
- $form = $("<form></form>");
+//Appending a form to the search container
+//Creating the search bar 
+$form = $("<form></form>");
  $form.append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
  $form.append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
  $('.search-container').append($form);
-
- function renderInfo(data) { 
+//Creating a function to create the image cards
+ function renderInfo(data) { //Creating a for loop for the 12 image cards to appear
+  console.log(data);
   for (let i = 0; i < 12; i++) { 
-    //   loadAndShowData();
+    // Creating variables that contain an array of objects defined inside of the data variable
      var imageLocation = data.results[i].picture.large; 
      console.log ("Hello")
      var name = data.results[i].name.first + " " + data.results[i].name.last;
@@ -38,7 +40,7 @@ let employees = [];
      $location = $('<p class="card-text cap">city, state</p>')
      $location.html(location)
      $cardInfoContainer.append($location)
-     $card.click(function(){
+     $card.click(function(){ //Applying an event handler to respond to a click
       employeeModal(i, data.results[i])
       }) 
   }        
@@ -54,7 +56,7 @@ let employees = [];
       /*  console.log(data.results[0].name);
        console.log(data.results[0].email);
        console.log(data.results[0]) */
-       renderInfo(data);
+       renderInfo(data); //Calling the function so that it runs and 12 employee cards appear
       
      }
    });
@@ -69,15 +71,15 @@ let employees = [];
    
    var location = employee.location.street + employee.location.city + ", " + employee.location.state + " " + employee.location.postcode; 
    var modalContent = '<button type="button id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
-  modalContent += "<img class='modal-img'  src='" + employee.picture.large + "'alt='profile picture'>";
-  modalContent += '<h3 id="name" class="modal-name cap">' + name + '</h3>';
-  modalContent += '<p class="modal-text">' + employee.email + '</p>';
-  modalContent +='<p class="modal-text cap">' + employee.location.city + '</p>';
-  modalContent += '<hr>';
-  modalContent += '<p class="modal-text">' + employee.phone + '</p>';
+   modalContent += "<img class='modal-img'  src='" + employee.picture.large + "'alt='profile picture'>";
+   modalContent += '<h3 id="name" class="modal-name cap">' + name + '</h3>';
+   modalContent += '<p class="modal-text">' + employee.email + '</p>';
+   modalContent +='<p class="modal-text cap">' + employee.location.city + '</p>';
+   modalContent += '<hr>';
+   modalContent += '<p class="modal-text">' + employee.phone + '</p>';
   
-  modalContent += '<p class="modal-text cap">' + location + '</p>';
-  modalContent += '<p class="modal-text"> Birthday: ' + dateOfBirth + '</p>';
+   modalContent += '<p class="modal-text cap">' + location + '</p>';
+   modalContent += '<p class="modal-text"> Birthday: ' + dateOfBirth + '</p>';
 
    modalContent += '</div>';
    
@@ -86,7 +88,7 @@ let employees = [];
    $('.modal').append(modalContent);
    $('.modal').css('display', 'block');
    console.log(index)
-   addEventListenerToModal(index);
+   addEventListenerToModal();
 
   }
 
@@ -101,20 +103,14 @@ let employees = [];
  }
 
 // //Getting the modal container box to close
- function addEventListenerToModal(idNumber) {
+ function addEventListenerToModal() {
   // console.log("Hello")
+  //Creating an event listener to close every time the modal close button is clicked
    $('.modal-close-btn').click(function() {
      $('.modal').css('display', 'none');
      $('.modal-container').remove();
    })
 
-   $('.next').click(function() {
-     var next = idNumber + 1;
-     if (idNumber <11) {
-       $('.modal-content').remove();
-       employeeModal(next);
-     }
-   })
  }
 
 
